b715f43bf59d876482b191f52433ff37daa613da
erxgithub <erxlink@gmail.com>
2018-01-23 19:46:46 -0500

    Added auto layout storyboard assignment.

---
+++ assignments/Auto Layout Storyboard/Auto Layout Storyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Auto Layout Storyboard
+//
+//  Created by Eric Gregor on 2018-01-23.
+//  Copyright © 2018 Eric Gregor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ assignments/Auto Layout Storyboard/Auto Layout Storyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Auto Layout Storyboard
+//
+//  Created by Eric Gregor on 2018-01-23.
+//  Copyright © 2018 Eric Gregor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
